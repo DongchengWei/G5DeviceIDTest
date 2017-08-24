1b9c86b28fb5f27576af3ee7a3b6d848d97f6a8f
DongchengWei <Dongcheng.Wei@desay-svautomotive.com>
2017-08-24 14:52:00 +0800

    reboot before run

--- GetLogs.bat
+++ GetLogs.bat
@@ -1,6 +1,6 @@
-adb -s 2e6408f0608200e2 shell su 0 svbugreport 1
-adb -s 2e6408f0608200e2 shell cp /sdcard/app_* /data/logs
-adb -s 2e6408f0608200e2 shell cp /sdcard/anr* /data/logs
+adb -s 2e6408f032520002 shell su 0 svbugreport 1
+adb -s 2e6408f032520002 shell cp /sdcard/app_* /data/logs
+adb -s 2e6408f032520002 shell cp /sdcard/anr* /data/logs
 mkdir %BUILD_NUMBER%
-adb -s 2e6408f0608200e2 pull /data/logs/ %WORKSPACE%\%BUILD_NUMBER%
+adb -s 2e6408f032520002 pull /data/logs/ %WORKSPACE%\%BUILD_NUMBER%
 copy %WORKSPACE%\monkey.txt %WORKSPACE%\%BUILD_NUMBER%
--- Prepare.bat
+++ Prepare.bat
@@ -1,5 +1,9 @@
-adb -s 2e6408f0608200e2 root
+adb -s 2e6408f032520002 root
 ping 127.0.0.1 -n 5 -w 1000 > nul
-adb -s 2e6408f0608200e2 shell rm -rf /sdcard/app_*
-adb -s 2e6408f0608200e2 shell rm -rf /sdcard/anr*
-adb -s 2e6408f0608200e2 shell rm -rf /data/logs/*
+adb -s 2e6408f032520002 shell reboot
+ping 127.0.0.1 -n 30 -w 1000 > nul
+adb -s 2e6408f032520002 root
+ping 127.0.0.1 -n 5 -w 1000 > nul
+adb -s 2e6408f032520002 shell rm -rf /sdcard/app_*
+adb -s 2e6408f032520002 shell rm -rf /sdcard/anr*
+adb -s 2e6408f032520002 shell rm -rf /data/logs/*
