e220801727a9edb09dcd62b501988fe9e6fed362
DongchengWei <Dongcheng.Wei@desay-svautomotive.com>
2017-08-28 14:43:28 +0800

    del monkey.txt after finish

--- GetLogs.bat
+++ GetLogs.bat
@@ -4,3 +4,4 @@ adb -s 2e6408f032520002 shell cp /sdcard/anr* /data/logs
 mkdir %BUILD_NUMBER%
 adb -s 2e6408f032520002 pull /data/logs/ %WORKSPACE%\%BUILD_NUMBER%
 copy %WORKSPACE%\monkey.txt %WORKSPACE%\%BUILD_NUMBER%
+del /F /S /Q %WORKSPACE%\monkey.txt
--- Prepare.bat
+++ Prepare.bat
@@ -7,3 +7,4 @@ ping 127.0.0.1 -n 5 -w 1000 > nul
 adb -s 2e6408f032520002 shell rm -rf /sdcard/app_*
 adb -s 2e6408f032520002 shell rm -rf /sdcard/anr*
 adb -s 2e6408f032520002 shell rm -rf /data/logs/*
+adb -s 2e6408f032520002 shell /system/bin/screencap -p /data/logs/BeforeMonkeyScreenshot.png
